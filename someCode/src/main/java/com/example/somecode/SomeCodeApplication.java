a669bc764e219b3b126cfdeba145e71983993fb6
andrey1510 <andrey-1510@outlook.com>
2022-09-16 18:05:18 +0300

    7 commit

--- someCode/src/main/java/com/example/somecode/SomeCodeApplication.java
+++ someCode/src/main/java/com/example/somecode/SomeCodeApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SomeCodeApplication {
 
-
+    public String s = "Some code";
+    int i;
 
     public static void main(String[] args) {
         SpringApplication.run(SomeCodeApplication.class, args);
