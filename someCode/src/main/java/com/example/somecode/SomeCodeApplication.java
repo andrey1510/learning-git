eb1e0b96920805252294ff1dfc1b96e58effc7fd
andrey1510 <andrey-1510@outlook.com>
2022-09-16 18:16:18 +0300

    10 commit

--- someCode/src/main/java/com/example/somecode/SomeCodeApplication.java
+++ someCode/src/main/java/com/example/somecode/SomeCodeApplication.java
@@ -8,6 +8,8 @@ public class SomeCodeApplication {
 
     public String s = "Some code";
     int i;
+    long l = 2;
+    char c;
 
     public static void main(String[] args) {
         SpringApplication.run(SomeCodeApplication.class, args);
